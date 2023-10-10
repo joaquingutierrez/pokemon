4adea0b9d6cadbe12055eccf8b561c3888fa367e
joaquingutierrez <joaquinguty@gmail.com>
2023-10-10 19:41:15 -0300

    Estilos: Terminada la parte de la pantalla de la pokedex, comenzando con la parte de los botones

--- src/componentsContainer/Pokedex/index.jsx
+++ src/componentsContainer/Pokedex/index.jsx
@@ -9,9 +9,28 @@ const Pokedex = ({ screen1, screen2 }) => {
                     </div>
                 </div>
                 <div className="pokedex_frame1">
+                    <div className="pokedex_circle_container">
+                        <div className="pokedex_circle"></div>
+                        <div className="pokedex_circle"></div>
+                    </div>
                     <div className="pokedex_screen1">
                         {screen1}
                     </div>
+                    <div className="pokedex_frame1_footer">
+                        <div className="pokedex_frame1_footer_circle"></div>
+                        <div className="pokedex_stripe_container">
+                            <div className="pokedex_stripe"></div>
+                            <div className="pokedex_stripe"></div>
+                            <div className="pokedex_stripe"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="pokedex_left_buttons_container">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div className="direction_buttons"></div>
                 </div>
             </div>
             <div className="pokedex_hinge"></div>
